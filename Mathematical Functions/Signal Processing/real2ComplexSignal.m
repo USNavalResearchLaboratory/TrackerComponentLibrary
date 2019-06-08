88f38ddc25c8d8e14b380664d48fe90853a1b37d
David Crouse <david.crouse@nrl.navy.mil>
2019-06-08 16:36:54 -0400

    Bug fix.

--- Mathematical Functions/Signal Processing/real2ComplexSignal.m
+++ Mathematical Functions/Signal Processing/real2ComplexSignal.m
@@ -1,7 +1,7 @@
 function z=real2ComplexSignal(x,M)
 %%REAL2COMPLEXSIGNAL Given the real in-phase part of a signal, this
 %          function returns a complex signal with in-phase and quadrature
-%          parts. This function can also simultanoeusly decimate the signal
+%          parts. This function can also simultaneously decimate the signal
 %          by 1/2 or interpolate the signal out by an integer multiple. The
 %          signal length must be divisible by 2 to perform decimation.
 %
@@ -11,27 +11,32 @@ function z=real2ComplexSignal(x,M)
 %          by 1/2. If this parameter is omitted or an empty matrix is
 %          passed, a default value of 1 is used.
 %
-%OUTPUTS: z The (M*N)X1 complex signal.
+%OUTPUTS: z The (M*N)X1 complex signal. If M=1, then the real part of z
+%           will match the real values in x (within finite precision
+%           bounds). If M~=1, then the real components will typically not
+%           perfectly match any of the interpoalted values.
 %
 %The algorithm is the "analytic" signal computation algorithm of [1]. The
 %paper focusses on the case where N is even. However, the generalization
-%for interpolation for N being odd is straightforward. This function can be
+%for interpolation for N being odd is straightforward. For M=1, the real
+%part of z equals x within finite precision limits. This function can be
 %used to take a real samples signal and return a "quadrature" component for
 %signal processing purposes. 
 %
 %EXAMPLE:
 % t=(1:100).';
 % x=exp(1j*2*pi*t/100);
-% real2ComplexSignal(real(x))-x
-%One will get a result that is numerically zero for all of the elements.
-%Note that using x=exp(-1j*2*pi*t/100), will, of course cause this function
-%to return the same output, as the real part of the signal can't express
-%the 180 degree phase shift. Thus, there is an ambiguity in the result.
-%Note that it also works with an odd-length signal:
+% max(abs(real2ComplexSignal(real(x))-x))
+%One will get a result that is as close to zero as one would expect with
+%finite precision limitations. zero for all of the elements. Note that
+%using x=exp(-1j*2*pi*t/100), will, of course cause this function to return
+%the same output, as the real part of the signal can't express the 180
+%degree phase shift. Thus, there is an ambiguity in the result. Note that
+%it also works with an odd-length signal:
 % t=(1:101).';
 % x=exp(1j*2*pi*t/101);
-% real2ComplexSignal(real(x))-x
-%One also gets numerically zero results.
+% max(abs(real2ComplexSignal(real(x))-x))
+%One also get a maximum error that is also close to zero.
 %
 %REFERENCES:
 %[1] S. L. Marple Jr., "Computing the discrete-time 'analytic' signal via
@@ -54,7 +59,7 @@ else
     if(mod(N,2)==0)
         z=ifft([X(1);2*X(2:(N/2));X(N/2+1);zeros(M*N-N/2-1,1)],M*N);
     else
-        z=ifft([X(1);2*X(2:((N+1)/2));X((N+1)/2+1);zeros(M*N-(N+1)/2-1,1)],M*N);
+        z=ifft([X(1);2*X(2:((N+1)/2));zeros(M*N-(N+1)/2,1)],M*N);
     end
 end
 end
